a2786c36a56d4f5f520a42e1f8a9b790625b03de
Karma Ghale <karmaghale@Karmas-Air-2.cable.rcn.com>
2023-05-23 22:24:39 -0400

    navbar started

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <div>Description</div>
+      <div>Short bio</div>
+      <div>Github Link</div>
+    </div>
+  );
+}
+
+export default About;
