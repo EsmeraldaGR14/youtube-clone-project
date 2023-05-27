cb4bd861e78bc3b4116a422cb5b73310aa3d8a8e
Esmeralda Gonzalez Rojo <esmeralda@Esmeraldas-Air.lan>
2023-05-27 11:34:54 -0400

    created new semi functional components.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import VideoCards from "./VideoCards";
 
 function SearchBar() {
-  return <div>SearchBar</div>;
+  const [searchValue, setSearchValue] = useState("");
+  function SearchItem(e) {
+    console.log(searchValue);
+  }
+
+  function SearchValueBar(e) {
+    setSearchValue(e.target.value);
+  }
+  return (
+    <>
+      <div>SearchBar</div>
+      <input
+        id="search"
+        type="text"
+        placeholder="Search..."
+        onChange={SearchValueBar}
+      />
+      <button onClick={SearchItem}>Search</button>
+
+      {searchValue && <VideoCards searchValue={searchValue} />}
+      {/* Video cards should apply conditional rendering => IF searchValue is not an empty string THEN display videos */}
+    </>
+  );
 }
 
 export default SearchBar;
